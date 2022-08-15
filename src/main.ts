20e78922611065bf2d57f4550cd5884a04e23e8e
Kaaleyah <41757315+Kaaleyah@users.noreply.github.com>
2022-08-15 15:21:31 +0300

    Imported firebase

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import './style.css'
 import { circle } from './init'
 import { joinRoom } from './socket'
+import { initFirebase } from './rtc';
 
 import { io } from "socket.io-client";
 
-var socket = io('10.254.127.14:3000');
+var socket = io('10.254.132.66:3000');
 
 circle(document.querySelector('#background')!)
+console.log(initFirebase());
+
 
 window.addEventListener('resize', () => {
   circle(document.querySelector('#background')!)
