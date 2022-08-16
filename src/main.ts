73272f12067dd1e7517b8555dfb9a4e91d3abb96
Kaaleyah <41757315+Kaaleyah@users.noreply.github.com>
2022-08-16 16:44:07 +0300

    Accept response now establishes RTC connection

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import './style.css'
 import { circle } from './init'
 import { joinRoom } from './socket'
-import { initFirebase, createAnswer, createOffer } from './rtc';
 
 import { io } from "socket.io-client";
 
-//var socket = io('10.254.132.66:3000');
+var socket = io('10.254.132.66:3000');
 
 circle(document.querySelector('#background')!)
 
@@ -13,18 +12,8 @@ window.addEventListener('resize', () => {
   circle(document.querySelector('#background')!)
 });
 
-document.querySelector('.create')?.addEventListener('click', () => {
-  createOffer('sa.txt')
-  console.log("Offer created");
-  
-})
-
 document.querySelector('form')?.addEventListener('submit', (e) => {
     e.preventDefault()
-    //joinRoom(socket)
+    joinRoom(socket)
 
-    const roomInput = document.querySelector('#roomID') as HTMLInputElement
-
-    createAnswer(roomInput.value)
-    console.log("Answer created");
 })
