7d41c2a578e3ac95cb81ca45749f45d794b2b40c
Furkan Can Baytemur <41757315+Kaaleyah@users.noreply.github.com>
2022-11-01 11:42:21 +0300

    Update rtc.ts

--- app/src/rtc.ts
+++ app/src/rtc.ts
@@ -5,13 +5,7 @@ import { getFirestore, doc, getDoc, collection, addDoc, onSnapshot, updateDoc, d
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA0dI875y3Yc87rnGqQDKsiizOxC6BxfBg",
-  authDomain: "plain-webrtc.firebaseapp.com",
-  projectId: "plain-webrtc",
-  storageBucket: "plain-webrtc.appspot.com",
-  messagingSenderId: "243377585279",
-  appId: "1:243377585279:web:21b64ad1d44a6e0079934a",
-  measurementId: "G-07B1J2JET4"
+  // Create a firebase project and paste your config in here
 };
 
 // Initialize Firebase
