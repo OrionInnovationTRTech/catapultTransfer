59fa3b1406722b744245f8eefe43bef5941f609a
Furkan Can Baytemur <41757315+Kaaleyah@users.noreply.github.com>
2023-01-06 15:18:21 +0300

    Help button added

--- app/src/main.ts
+++ app/src/main.ts
@@ -9,9 +9,18 @@ import { io } from "socket.io-client";
 var socket = io('https://sea-turtle-app-qrpgx.ondigitalocean.app');
 
 var introDismiss = document.querySelector('#introDismiss') as HTMLInputElement;
+var helpBtn = document.querySelector('#helpBtn') as HTMLInputElement;
+
+if (document.cookie.indexOf("visited=true") >= 0) {
+  document.querySelector('.intro').classList.add("hideIntro");
+}
 
 introDismiss.addEventListener('click', () => {
   document.querySelector('.intro')?.classList.add('hideIntro');
+})
+
+helpBtn.addEventListener('click', () => {
+  document.querySelector('.intro')?.classList.remove('hideIntro');
 })
 
 
