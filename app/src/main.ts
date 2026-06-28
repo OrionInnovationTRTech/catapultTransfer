4cf999662f627a059088109b71232afa1434d2f1
Furkan Can Baytemur <41757315+Kaaleyah@users.noreply.github.com>
2026-06-28 21:26:24 +0300

    Update socket connection URL to Railway app

--- app/src/main.ts
+++ app/src/main.ts
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 
 // Change this according to your own local IP
 //var socket = io('http://192.168.1.9:3000');
-var socket = io('https://sea-turtle-app-qrpgx.ondigitalocean.app');
+var socket = io('https://catapulttransfer-production.up.railway.app');
 
 var introDismiss = document.querySelector('#introDismiss') as HTMLInputElement;
 var helpBtn = document.querySelector('#helpBtn') as HTMLInputElement;
